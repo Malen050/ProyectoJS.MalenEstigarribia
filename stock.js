bd3e3169752e7e6936d1a498f9f56a9100253c02
Malen Estigarribia <malenchuu87@gmail.com>
2023-03-20 21:25:21 -0300

    nuevos cambios

---
+++ stock.js
@@ -0,0 +1,15 @@
+let stockProductos = [
+
+    { id: 1, nombre: 'Anillo de oro', precio: 1500, img: src="https://images.unsplash.com/photo-1589207212797-cfd546dea0fe?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", cantidad: 1 },
+    
+    { id: 2, nombre: 'Anillo de Flor', precio: 900, img: src= "https://images.unsplash.com/photo-1602751584552-8ba73aad10e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1086&q=80", cantidad: 1 },
+    
+    { id: 3, nombre: 'Collar de diamantes', precio: 5000, img: src= "https://images.unsplash.com/photo-1547738238-5ddb16bef15f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", cantidad: 1 },
+    
+    { id: 4, nombre: 'Piedra amatista', precio: 650, img: src= "https://images.unsplash.com/photo-1597177586824-33bda0c29325?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80", cantidad: 1 },
+    
+    { id: 5, nombre: 'Corona', precio: 10000, img: src="https://images.unsplash.com/photo-1474169482634-9d0381a70510?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80", cantidad: 1 },
+    
+    { id: 6, nombre: 'Collar piedra', precio: 800, img: src="https://images.unsplash.com/photo-1599643477877-530eb83abc8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", cantidad: 1 },
+    
+];
